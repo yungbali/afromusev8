ea9427e9e1b5cb19451617c8cdc5e7be7f018ccf
yungbali <info.prodbybali@gmail.com>
2024-12-09 11:32:40 +0100

    next.config.js setting for standalone

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
